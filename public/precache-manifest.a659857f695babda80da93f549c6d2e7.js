f6b930d500420569f34b71544791880adaed3f62
nkosinathintuli <nkosinathintuli21@gmail.com>
2020-03-13 14:07:04 +0200

    fixed commit issues

---
+++ public/precache-manifest.a659857f695babda80da93f549c6d2e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae21195b36d785aefdf5e028011b236c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4ab7d426b9262e09733",
+    "url": "/static/css/main.9b0cec6c.chunk.css"
+  },
+  {
+    "revision": "590cf06240b094f0afef",
+    "url": "/static/js/2.c211763c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c211763c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4ab7d426b9262e09733",
+    "url": "/static/js/main.fff72a60.chunk.js"
+  },
+  {
+    "revision": "8932fdb2331e7d4035e6",
+    "url": "/static/js/runtime-main.6c2ae1b3.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/static/media/pokeball.f10bf078.png"
+  }
+]);
